6bab275fe1c5e2c29f0225cb4ec46eee55e22948
wasborn <32726984+wasborn@users.noreply.github.com>
2017-10-17 23:05:16 +0700

    Update Nhu.py

--- Nhu.py
+++ Nhu.py
@@ -94,12 +94,12 @@ helpMessage ="""        =====[C̶̲̅ᴏ̶̲̅ᴍ̶̲̅ᴍ̶̲̅ᴀ̶̲̅ɴ̲̅�
 """
 helo=""
 
-KAC=[cl,ki,kk,kc]["uda539eeaf99baac3a3bf45c9d92f1cad",]
+KAC=[cl,ki,kk,kc]
 mid = cl.getProfile().mid
-Amid = kk.getProfile().mid["uda539eeaf99baac3a3bf45c9d92f1cad",]
-Bmid = ki.getProfile().mid["uda539eeaf99baac3a3bf45c9d92f1cad",]
-Cmid = kc.getProfile().mid["uda539eeaf99baac3a3bf45c9d92f1cad",]
-Bots = [mid,Amid,Bmid,Cmid,]["uda539eeaf99baac3a3bf45c9d92f1cad",]
+Amid = kk.getProfile().mid
+Bmid = ki.getProfile().mid
+Cmid = kc.getProfile().mid
+Bots = [mid,Amid,Bmid,Cmid,]
 admin = ["u2c7f708769a2eb35d9ae9f73cd366e0b","ID:0647796747",]
 staff = [""]
 adminMID = [""]
