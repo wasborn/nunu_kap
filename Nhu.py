4bdac1e13602e490646d09697d196b7c8c98bc44
wasborn <32726984+wasborn@users.noreply.github.com>
2017-10-18 20:47:00 +0700

    ##

--- Nhu.py
+++ Nhu.py
@@ -98,9 +98,9 @@ KAC=[cl,ki,kk,kc]
 mid = cl.getProfile().mid
 Amid = kk.getProfile().mid
 Bmid = ki.getProfile().mid
-Cmid = kc.getProfile().mid
-Bots = [mid,Amid,Bmid,Cmid,]
-admin = ["u2c7f708769a2eb35d9ae9f73cd366e0b",]
+CMID = KC   getProfile ().mid
+Bots = [mid , uda539eeaf99baac3a3bf45c9d92f1cad , Bmid , Cmid,]
+admin = [ "u2c7f708769a2eb35d9ae9f73cd366e0b" ,]
 staff = [""]
 adminMID = [ "" ]
 
